a444c73baeaa1c6d752e0731c4639ad568c14eff
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-05-31 19:56:42 +0500

    004_LongestCommonPrefix

--- LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/004_LongestCommonPrefix.cs
+++ LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/004_LongestCommonPrefix.cs
@@ -2,25 +2,25 @@
 
 internal class _004_LongestCommonPrefix
 {
-    //public string LongestCommonPrefix(string[] strs)
-    //{
-    //    var shortestWord = strs[0];
-    //    var toReturn = "";
-    //    for (int i = 1; i < strs.Length; i++)
-    //    {
-    //        if (shortestWord.Length > strs[i].Length)
-    //        {
-    //            shortestWord = strs[i];
-    //        }
-    //    }
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var shortestWord = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+        {
+            if (shortestWord.Length > strs[i].Length)
+            {
+                shortestWord = strs[i];
+            }
+        }
 
-    //    for (int i = 0; i < shortestWord.Length; i++)
-    //    {
-    //        if (strs.All(w => w[i] == shortestWord[i]))
-    //            toReturn += shortestWord[i];
-    //        else
-    //            return toReturn;
-    //    }
-    //    return toReturn;
-    //}
+        var toReturn = "";
+        for (int i = 0; i < shortestWord.Length; i++)
+        {
+            if (strs.All(w => w[i] == shortestWord[i]))
+                toReturn += shortestWord[i];
+            else
+                return toReturn;
+        }
+        return toReturn;
+    }
 }
