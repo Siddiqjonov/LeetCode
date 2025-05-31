631ae785c21a4d7ee41d1ea587b0d44b18b8000f
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-05-31 19:53:31 +0500

    007_ReverseVowelsOfAtring

---
+++ LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/007_ReverseVowelsOfAtring.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace _01_LeetCodeEasyProblemSolutions.Top_100;
+
+internal class _007_ReverseVowelsOfAtring
+{
+    public string ReverseVowels(string s)
+    {
+        string vowels = string.Empty;
+        var latters = new StringBuilder(s);
+
+        foreach (char l in s)
+        {
+            if (char.IsLetter(l))
+            {
+                switch (l)
+                {
+                    case 'a': case 'e': case 'i': case 'o': case 'u':
+                    case 'A': case 'E': case 'I': case 'U': case 'O':
+                    {
+                        vowels += l;
+                        break;
+                    }
+                }
+            }
+        }
+
+        vowels = new string(vowels.Reverse().ToArray());
+
+        int index = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            switch (s[i])
+            {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                case 'A': case 'E': case 'I': case 'O': case 'U':
+                {
+                    latters[i] = vowels[index];
+                    index++;
+                    break;
+                }
+            }
+        }
+
+        return latters.ToString();
+    }
+}
