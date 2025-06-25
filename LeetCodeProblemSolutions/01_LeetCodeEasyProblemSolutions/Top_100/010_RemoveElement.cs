1362db4a0c50d2a13cca962a4bec1a06fe45ee29
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-06-25 23:26:09 +0500

    10th problem has been solved

---
+++ LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/010_RemoveElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LeetCodeEasyProblemSolutions.Top_100
+{
+    internal class _010_RemoveElement
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            var numswithoutval = nums.Where(n => n != val).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i < numswithoutval.Length)
+                {
+                    nums[i] = numswithoutval[i];
+                }
+                else
+                {
+                    nums[i] = 0;
+                }
+            }
+            return numswithoutval.Length;
+        }
+    }
+}
