b96ffea3199ef91c9c79868f5c4f36366a697c78
Your Name <you@example.com>
2025-08-25 17:30:03 +0500

    first version of the task

---
+++ LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/011_FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LeetCodeEasyProblemSolutions.Top_100
+{
+    internal class _011_FindTheIndexOfTheFirstOccurrenceInAString
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
