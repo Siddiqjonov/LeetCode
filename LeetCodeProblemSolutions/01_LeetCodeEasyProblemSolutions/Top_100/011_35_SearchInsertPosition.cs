71465f4c6248712d9dd9e600fd84cb3d7133d85d
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-09-27 18:25:42 +0500

    problem: 35_SearchInsertPosition

---
+++ LeetCodeProblemSolutions/01_LeetCodeEasyProblemSolutions/Top_100/011_35_SearchInsertPosition.cs
@@ -0,0 +1,22 @@
+﻿namespace _01_LeetCodeEasyProblemSolutions.Top_100;
+
+internal class _011_35_SearchInsertPosition
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var small = nums.Where(x => x < target).DefaultIfEmpty().Max();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+
+            if (nums[i] == small && small != 0)
+            {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+}
---
+++ LeetCodeProblemSolutions/02_LeetCodeMediumProblemSolutions/Top_100/001_71_Simplify_Path.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LeetCodeMediumProblemSolutions.Top_100
+{
+    internal class _001_71_Simplify_Path
+    {
+    }
+}
